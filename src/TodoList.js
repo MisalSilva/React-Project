763550e8491c3796ac02c2645e432bf7edcbb7d7
MisalSilva <misal.20230183@iit.ac.lk>
2024-11-29 15:44:40 +0530

    Add Files

---
+++ src/TodoList.js
@@ -0,0 +1,14 @@
+function TodoList() {
+    return(
+        <div>
+            <h2>My Tasks</h2>
+            <ul>
+                <li>Learn React</li>
+                <li>build Components</li>
+                <li>Pass Props</li>
+            </ul>
+        </div>
+        
+    );    
+}
+export default TodoList;
